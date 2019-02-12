cda2895719932dedc89f7f1507101704394f5180
linhaomiao <linhaomiao@bluemoon.com.cn>
2019-02-12 17:29:59 +0800

    去掉application.yml里面的服务器配置(因为采用外部Tomcat部署)

--- src/main/java/com/hmlin5/demo/upload/UploadController.java
+++ src/main/java/com/hmlin5/demo/upload/UploadController.java
@@ -24,7 +24,8 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @Controller
